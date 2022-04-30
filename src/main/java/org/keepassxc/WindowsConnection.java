6a6523d065fc8ae97b9319db77ae321f2bbf6e58
Ralph Plawetzki <ralph@purejava.org>
2022-04-30 10:26:34 +0200

    Replace RandomAccessFile with AsynchronousFileChannel to fix deadlock problems on Windows

--- src/main/java/org/keepassxc/WindowsConnection.java
+++ src/main/java/org/keepassxc/WindowsConnection.java
@@ -6,16 +6,28 @@ import org.purejava.KeepassProxyAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.Future;
 
 public class WindowsConnection extends Connection {
 
     private static final Logger log = LoggerFactory.getLogger(WindowsConnection.class);
 
-    private RandomAccessFile pipe;
+    private final int BUFFER_SIZE = 8192;
+    private AsynchronousFileChannel pipe;
+    private final ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+    private final Charset charset = StandardCharsets.UTF_8;
+    private final CharsetDecoder charsetDecoder = charset.newDecoder();
+    private final CharBuffer charBuffer = CharBuffer.allocate(BUFFER_SIZE);
 
     /**
      * Connect to the KeePassXC proxy via a Windows named pipe the proxy has opened.
@@ -25,9 +37,9 @@ public class WindowsConnection extends Connection {
     @Override
     public void connect() throws IOException {
         try {
-            pipe = new RandomAccessFile("\\\\.\\pipe\\" + PROXY_NAME + "_" + System.getenv("USERNAME"),
-                    "rw");
-        } catch (FileNotFoundException e) {
+            Path path = Paths.get("\\\\.\\pipe\\" + PROXY_NAME + "_" + System.getenv("USERNAME"));
+            pipe = AsynchronousFileChannel.open(path, StandardOpenOption.READ, StandardOpenOption.WRITE);
+        } catch (IOException e) {
             log.error("Cannot connect to proxy. Is KeepassXC started?");
             throw e;
         }
@@ -42,25 +54,27 @@ public class WindowsConnection extends Connection {
     @Override
     protected void sendCleartextMessage(String msg) throws IOException {
         log.trace("Sending message: {}", msg);
-        pipe.write(msg.getBytes(StandardCharsets.UTF_8));
+        pipe.write(ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8)), 0);
     }
 
     @Override
     protected JSONObject getCleartextResponse() {
-        int c;
-        var raw = "";
-        do {
-            try {
-                c = pipe.read();
-                raw += (char) c;
-            } catch (IOException e) {
-                log.error(e.toString(), e.getCause());
-                return new JSONObject();
-            }
-        } while (c != 125); // end of transmission
+        var raw = new StringBuilder();
+        long position = 0;
+        Future<Integer> operation = pipe.read(buffer, position);
+        while (!operation.isDone());
+        buffer.flip();
+        charsetDecoder.decode(buffer, charBuffer, true);
+        charBuffer.flip();
+        raw.append(charBuffer);
+        buffer.compact();
+        charBuffer.clear();
         log.trace("Reading message: {}", raw);
         try {
-            return new JSONObject(raw);
+            var s = raw.toString();
+            // Test, if we received more than one message with the last read
+            if (s.length() - s.replace("}", "").length() > 1) throw new JSONException("");
+            return new JSONObject(raw.toString());
         } catch (JSONException e) {
             log.error("Message corrupted. Received: {}", raw);
             return new JSONObject();
