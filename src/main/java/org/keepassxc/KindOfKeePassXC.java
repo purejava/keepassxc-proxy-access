2684a1b9eb16a0acce295a35ed6fa611c3c22df4
Ralph Plawetzki <ralph@purejava.org>
2022-11-16 23:25:36 +0100

    Fix recognition

--- src/main/java/org/keepassxc/KindOfKeePassXC.java
+++ src/main/java/org/keepassxc/KindOfKeePassXC.java
@@ -12,6 +12,7 @@ public class KindOfKeePassXC {
         var processHandle = ProcessHandle.allProcesses()
                 .filter(ph -> ph.info().command().isPresent() && ph.info().command().get().contains("KeePassXC"))
                 .findFirst();
+
         if (processHandle.isPresent() && processHandle.get().info().command().get().contains("AppImage")) {
             LOG.debug("Found running KeePassXC AppImage");
             return Optional.of(KeePassXCType.AppImage);
@@ -20,22 +21,20 @@ public class KindOfKeePassXC {
         processHandle = ProcessHandle.allProcesses()
                 .filter(ph -> ph.info().commandLine().isPresent() && ph.info().commandLine().get().contains("keepassxc"))
                 .findFirst();
-        if (processHandle.isPresent()) {
-            LOG.debug("Found running KeePassXC installed from repository");
-            return Optional.of(KeePassXCType.Repo);
-        }
 
-        processHandle = ProcessHandle.allProcesses()
-                .filter(ph -> ph.info().command().isPresent() && ph.info().command().get().contains("keepassxc"))
-                .findFirst();
-        if (processHandle.isPresent() && processHandle.get().info().command().get().contains("app")) {
+        if (processHandle.isPresent() && processHandle.get().info().commandLine().get().contains("bwrap")) {
             LOG.debug("Found running KeePassXC installed via Flatpak");
             return Optional.of(KeePassXCType.Flatpak);
         }
 
-        if (processHandle.isPresent() && processHandle.get().info().command().get().contains("snap")) {
+        if (processHandle.isPresent() && processHandle.get().info().commandLine().get().contains("snap")) {
             LOG.debug("Found running KeePassXC installed via Snap");
             return Optional.of(KeePassXCType.Snap);
+        }
+
+        if (processHandle.isPresent() && processHandle.get().info().commandLine().get().contains("bin")) {
+            LOG.debug("Found running KeePassXC installed from repository");
+            return Optional.of(KeePassXCType.Repo);
         }
 
         LOG.debug("Could not find running KeePassXC application");
