ccb75dc73c5be20a8243603ef947258301cc07ad
Ralph Plawetzki <ralph@purejava.org>
2022-11-17 07:27:24 +0100

    Add comments with commands to search for

--- src/main/java/org/keepassxc/KindOfKeePassXC.java
+++ src/main/java/org/keepassxc/KindOfKeePassXC.java
@@ -13,6 +13,7 @@ public class KindOfKeePassXC {
                 .filter(ph -> ph.info().command().isPresent() && ph.info().command().get().contains("KeePassXC"))
                 .findFirst();
 
+        ///home/<user>/Downloads/KeePassXC-2.7.4-x86_64.AppImage
         if (processHandle.isPresent() && processHandle.get().info().command().get().contains("AppImage")) {
             LOG.debug("Found running KeePassXC AppImage");
             return Optional.of(KeePassXCType.AppImage);
@@ -22,16 +23,19 @@ public class KindOfKeePassXC {
                 .filter(ph -> ph.info().commandLine().isPresent() && ph.info().commandLine().get().contains("keepassxc"))
                 .findFirst();
 
+        ///usr/bin/bwrap --args 38 keepassxc-wrapper
         if (processHandle.isPresent() && processHandle.get().info().commandLine().get().contains("bwrap")) {
             LOG.debug("Found running KeePassXC installed via Flatpak");
             return Optional.of(KeePassXCType.Flatpak);
         }
 
+        ///snap/keepassxc/1645/usr/bin/keepassxc
         if (processHandle.isPresent() && processHandle.get().info().commandLine().get().contains("snap")) {
             LOG.debug("Found running KeePassXC installed via Snap");
             return Optional.of(KeePassXCType.Snap);
         }
 
+        ///usr/bin/keepassxc
         if (processHandle.isPresent() && processHandle.get().info().commandLine().get().contains("bin")) {
             LOG.debug("Found running KeePassXC installed from repository");
             return Optional.of(KeePassXCType.Repo);
